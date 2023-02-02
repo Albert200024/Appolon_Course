ce6a183f94b46d5c5bcb98987ad7befc1fccb492
Albert200024 <ghumashyan.albert@inbox.ru>
2023-02-02 17:07:56 +0400

    finish all figma

---
+++ JavaScript/lesson 1/js/script.js
@@ -0,0 +1,26 @@
+// document.write("Hello World");
+
+
+// var anun = "name ";
+// var azganun = "surname ";
+// var age = 20;
+
+// document.write('<h1>Hello ' + anun + azganun + age + ' tarekan</h1');
+
+// function hashvel(){
+// 	var x = Number(document.getElementById("dram").value);
+// 	// console.log(x)
+// 	var result = x / 500;
+// 	// console.log(result)
+// 	document.write("<h1> " + x + " dram = " + result + "$</h1>")
+// }
+
+function hashvel(){
+	var x = Number(document.getElementById("tiv1").value);
+	var y = Number(document.getElementById("tiv2").value);
+	var res = x + y;
+	console.log(res);
+	document.write("<h1>" + res + "</h1>")
+}
+
+alert (10 + 10 - 10 + "10")
