bebf21e1c6a029979db087c9d2a1f519b22ddb15
Albert200024 <ghumashyan.albert@inbox.ru>
2023-02-20 22:25:54 +0400

    add homework 6

--- JavaScript/HTML JS Select value_loop-lesson5/JS homework 5/js/script.js
+++ JavaScript/HTML JS Select value_loop-lesson5/JS homework 5/js/script.js
@@ -1,23 +1,25 @@
 // Exercise 1
 function hashvel(){
-    let patasxan = document.getElementById("patasxan")
+    let patasxan = document.getElementById("patasxan");
     let course_count = Number(document.getElementById("course_count").value);
     let course_front = document.getElementById("cours_frontend").value;
     let course_back =  document.getElementById("cours_backend").value;
     let course_designer = document.getElementById("cours_designer").value;
     let result;
+    var zexch = 0;
+    var zexchvacGin;
 
     if(isNaN(course_count) == false){
         result = course_count * (Number(course_front) + Number(course_back) + Number(course_designer));
         if(result >  299999){
-            var zexch = (result / 100) * 30
-            result -= zexch
+            zexch = (result / 100) * 30;
+            zexchvacGin = result - zexch;
         }
     }
 
-    if(result > 299999){
-        patasxan.innerHTML = "Հարգելի ուսանող, Դուք պետք է վճարեք " + result +  " դրամ, սակայն ստանում եք 30% զեղչ և վերջնական գումարը կազմում է " + zexch + " դրամ։" 
-    } else if(isNaN(course_count) == false){
+    if(zexch > 0){
+        patasxan.innerHTML = "Հարգելի ուսանող, Դուք պետք է վճարեք " + result +  " դրամ, սակայն ստանում եք 30% զեղչ և վերջնական գումարը կազմում է " + zexchvacGin + " դրամ։" 
+    } else if(zexch == 0 ){
         patasxan.innerHTML = "Հարգելի ուսանող, Դուք պետք է վճարեք " + result +  " դրամ:"
     } else {
         patasxan.innerHTML ="Հարգելի ուսանող պետք է անպայման մուքագրեք թիվ։"
@@ -30,7 +32,7 @@ let eranishTver = document.getElementById("eranishTver");
 let result1 = "";
 
 for(let i = 0; i < 1000; i++){
-    if(i > 99 && i % 2 == 0){
+    if(i >= 100 && i % 2 == 0){
          result1 += " " + i;
     }
 }
@@ -38,5 +40,37 @@ for(let i = 0; i < 1000; i++){
 eranishTver.innerHTML=result1;
 
 // Exercise 3
+let erknishTver = document.getElementById("erknishTver");
+let result2 = "";
+
+for(let i = 10; i < 100; i++){
+     if (i % 3 == 0 && i % 5 == 0){
+        result2 += " FIZBAZ ";
+    } else if(i % 3 == 0){
+        result2 += " FIZ ";
+    } else if( i % 5 == 0){
+        result2 += " BAZ ";
+    } else {
+        result2 += " " + i;
+    }
+}
+
+erknishTver.innerHTML = result2;
 
 // Exercise 4
+let fibonachiPatasxan = document.getElementById("fibonachiPatasxan");
+let a = 0;
+let b = 1;
+let gumar;
+let print = "";
+
+for(let i = 0; i < 100; i++){
+    gumar = a + b;
+    a = b;
+    b = gumar;
+    print += " " + a;
+}
+ 
+if(print != ""){
+    fibonachiPatasxan.innerHTML = print;
+}
---
+++ JavaScript/HTMLJS Array/JS lesson6/js/script.js
@@ -0,0 +1,40 @@
+// var anun = ["name1", "name2", "name3", "name4"];
+// console.log(anun[5]);
+// document.write("<h1>" + anun[0] + "</h1>");
+// document.write("<h1>" + anun[1] + "</h1>");
+// document.write("<h1>" + anun[2] + "</h1>");
+// document.write("<h1>" + anun[3] + "</h1>");
+// document.write("<h1>" + anun[4] + "</h1>");
+
+
+// for(let i = 0; i < 4; i++){
+//   document.write("<h1>" + anun[i] + "</h1>");
+// }
+
+// anun[2] = "nameX";
+// console.log(anun);
+
+// avelacnel amenaverjic
+// anun.push("nameX");
+
+// avelacnel skzbic;
+// anun.unshift("name0");
+
+// jnjel skzbic;
+// anun.shift("");
+
+// jnjel amenaverjic;
+// anun.pop();
+// 1tiv qanak, 2tiv qani het korcni 1 tvi hashvarkic heto, 3argumente nshanakum e incg avelacnenq
+// anun.splice(2, 2)
+// console.log(anun); 
+
+// let tiv = [12, 112, 7, 9, 5, -12, 98, 25, 22, 15, 105];
+
+// var max = tiv[0];
+// for(let i = 0; i < tiv.length; i++){
+// 	if(tiv[i] >= max){
+// 		max = tiv[i]
+// 	}
+// }
+// console.log(max)
