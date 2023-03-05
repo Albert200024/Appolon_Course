43586a06c00e6d842baddb9086d8d5e8a15e22b2
Albert200024 <ghumashyan.albert@inbox.ru>
2023-03-06 00:41:25 +0400

    hello

--- JavaScript/HTMLJS Ֆունկցիոնալ_ծրագրավորում_String_func-lesson10/JS homework10/js/script.js
+++ JavaScript/HTMLJS Ֆունկցիոնալ_ծրագրավորում_String_func-lesson10/JS homework10/js/script.js
@@ -30,6 +30,25 @@ let sumArr = sum([1, 2, 3, 4, 5]);
 console.log("Exersize2 :", sumArr);
 
 // Exersize 3
+function isIsogram(text){
+  let text1 = text.toLowerCase();
+  let arrText = text.split("");
+  let count = 0;
+
+  for(let i = 0; i < text1.length; i++){
+      let x = text1[i]
+      for(let j = i + 1; j < arrText.length ; j++){
+          if(x == arrText[j]) count++
+      }
+  }
+
+  return count == 0 ? true : false
+}
+
+let isIsogramResult = isIsogram("Armen");
+console.log("Exersize3 :", isIsogramResult);
+
+// Exersize 4
 function replaceV2(text, a, b){
   let arrText = text.split("");
   
@@ -45,12 +64,12 @@ function replaceV2(text, a, b){
 }
 
 let replaceText = replaceV2("i heat js", "heat", "love");
-console.log("Exersize3 :", replaceText); 
+console.log("Exersize4 :", replaceText); 
 
-// Exersize 4
+// Exersize 5
 function isPalindrom(text){
   return text == text.split("").reverse().join("") ? true : false;
 }
 
-let isPalindromResult = isPalindrom("qaxaq");
-console.log("Exersize4 :", isPalindromResult);
+let isPalindromResult = isPalindrom("name");
+console.log("Exersize5 :", isPalindromResult);
