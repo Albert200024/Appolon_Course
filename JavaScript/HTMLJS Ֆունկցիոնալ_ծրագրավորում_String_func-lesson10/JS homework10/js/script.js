bb039d91e948eb94fc06bad540450496e0220c45
Albert200024 <ghumashyan.albert@inbox.ru>
2023-03-06 23:11:56 +0400

    fisj

--- JavaScript/HTMLJS Ֆունկցիոնալ_ծրագրավորում_String_func-lesson10/JS homework10/js/script.js
+++ JavaScript/HTMLJS Ֆունկցիոնալ_ծրագրավորում_String_func-lesson10/JS homework10/js/script.js
@@ -1,52 +1,52 @@
 // Exersize 1
-function countA(text){
-   let cout = 0;
-
-   let textArr = text.split("");
-   for(let i = 0; i < textArr.length; i++){
-       if(textArr[i] == "a"){
-         cout++;
-       }
-   }
-
-   return cout;
-}
-
-let countWordA = countA("Name Anna");
-console.log("Exersize1 :", countWordA);
+// function countA(text){
+//    let cout = 0;
+//
+//    let textArr = text.split("");
+//    for(let i = 0; i < textArr.length; i++){
+//        if(textArr[i] == "a"){
+//          cout++;
+//        }
+//    }
+//
+//    return cout;
+// }
+//
+// let countWordA = countA("Name Anna");
+// console.log("Exersize1 :", countWordA);
 
 // Exersize 2
-function sum(arr){
-  let sum = 0;
-
-  for(let i = 0; i < arr.length; i++){
-     sum += arr[i];
-  }
-
-  return sum;
-}
-
-let sumArr = sum([1, 2, 3, 4, 5]);
-console.log("Exersize2 :", sumArr);
+// function sum(arr){
+//   let sum = 0;
+//
+//   for(let i = 0; i < arr.length; i++){
+//      sum += arr[i];
+//   }
+//
+//   return sum;
+// }
+//
+// let sumArr = sum([1, 2, 3, 4, 5]);
+// console.log("Exersize2 :", sumArr);
 
 // Exersize 3
-function isIsogram(text){
-  let text1 = text.toLowerCase();
-  let arrText = text.split("");
-  let count = 0;
-
-  for(let i = 0; i < text1.length; i++){
-      let x = text1[i];
-      for(let j = i + 1; j < arrText.length ; j++){
-          if(x == arrText[j]) count++
-      }
-  }
-
-  return count == 0 ? true : false;
-}
-
-let isIsogramResult = isIsogram("Armen");
-console.log("Exersize3 :", isIsogramResult);
+// function isIsogram(text){
+//   let text1 = text.toLowerCase();
+//   let arrText = text.split("");
+//   let count = 0;
+//
+//   for(let i = 0; i < text1.length; i++){
+//       let x = text1[i];
+//       for(let j = i + 1; j < arrText.length ; j++){
+//           if(x == arrText[j]) count++
+//       }
+//   }
+//
+//   return count == 0 ? true : false;
+// }
+//
+// let isIsogramResult = isIsogram("Armen");
+// console.log("Exersize3 :", isIsogramResult);
 
 // Exersize 4
 function replaceV2(text, a, b){
@@ -59,7 +59,7 @@ function replaceV2(text, a, b){
   if(!text.includes(a)){
       return arrText.join("")
   } else {
-    for(let i = 0; i < a.length - 1; i++){
+    for(let i = 0; i < a.length; i++){
          if(i == 0) {
              x = text.indexOf(a[i]);
              y = text.indexOf(a[i]);
@@ -74,23 +74,30 @@ function replaceV2(text, a, b){
               newArr.push(b);
               break
           }
-      }
+    }
 
-    for(let i = y + 1; i < text.length; i++){
+    for(let i = y; i < text.length; i++){
         newArr.push(text[i])
     }
   }
 
-  return newArr.join("")
+  return newArr.join("");
+}
+
+function  replaceV3(text, a, b){
+    return text.replace(a, b)
 }
 
 let replaceText = replaceV2("i heat js", "heat", "love")
 console.log("Exersize4 :", replaceText);
 
-// Exersize 5
-function isPalindrom(text){
-  return text == text.split("").reverse().join("") ? true : false;
-}
+let replaceText1 = replaceV3("i heat js", "heat", "love")
+console.log("Exersize4-V1 :", replaceText1);
 
-let isPalindromResult = isPalindrom("name");
-console.log("Exersize5 :", isPalindromResult);
+// Exersize 5
+// function isPalindrom(text){
+//   return text == text.split("").reverse().join("") ? true : false;
+// }
+//
+// let isPalindromResult = isPalindrom("qaxaq");
+// console.log("Exersize5 :", isPalindromResult);
