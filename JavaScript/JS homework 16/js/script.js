0352c6d42212dbc25d37fcde95a74ef186c67cb7
Albert200024 <ghumashyan.albert@inbox.ru>
2023-04-04 12:05:31 +0400

    finish homework 16

--- JavaScript/JS homework 16/js/script.js
+++ JavaScript/JS homework 16/js/script.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
-    $(document).on("click", ".btn_createSelecter", function(){
-        console.log("hayer")
-        $(document).off("click", ".btn_createSelecter")
+    $(".btn_createSelector").click( function(){
+        $(".selectBlock").empty()
+        let elem = $("<select class='sel'></select>");
+        elem.css("margin-bottom", "10px")
+        elem.appendTo(".selectBlock")
+
+        for(let i = 0; i < 5; i++){
+            let num = parseInt(Math.random() * 100)
+            let option = $("<option></option>");
+            option.html(num);
+            option.val(num);
+            option.appendTo(".sel");
+        }
+
+    })
+
+    $(".del_selector").click(function (){
+        $(".selectBlock").empty()
+    })
+
+    let arr = []
+
+    $(".save").click(function (e){
+         let inputVal = $("input").val();
+         arr.push(inputVal)
+    })
+
+    $(".addSelector").click(function (){
+        $(".selBlock").empty()
+        let sel = $("<select class='sel'></select>")
+
+        sel.css("margin-top", "8px")
+        sel.appendTo(".selBlock");
+
+        for(let i = 0; i < arr.length; i++){
+                let op = $("<option></option>")
+                op.html(arr[i])
+                op.appendTo(".sel")
+         }
     })
 })
