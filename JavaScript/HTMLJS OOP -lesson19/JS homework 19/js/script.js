45f73358d05ea9dc1625b540585f4df179048e76
Albert200024 <ghumashyan.albert@inbox.ru>
2023-04-21 10:31:43 +0400

    as

--- JavaScript/HTMLJS OOP -lesson19/JS homework 19/js/script.js
+++ JavaScript/HTMLJS OOP -lesson19/JS homework 19/js/script.js
@@ -1,37 +1,80 @@
 $(document).ready(function (){
     class Uxankyun {
         constructor(a, b) {
-            this.a = a
-            this.b = b
+            this.a = a;
+            this.b = b;
         }
 
         paragic(){
-            return (this.a + this.a) + (this.b + this.b)
+            return (this.a + this.a) + (this.b + this.b);
         }
         makeres(){
-            return this.a * this.b
+            return this.a * this.b;
         }
 
         draw(){
-            let div = $(`<div style="background-color: red; width: ${this.b}px; height: ${this.a}px"> </div>`);
+            let div = $(`<div> </div>`);
+            div.css("display", "flex");
+
+            for(let i = 0; i < this.a; i++){
+                let syun = $("<span></span>");
+                for(let j = 1; j <= this.b; j++){
+                   if(i == 0){
+                       let astxanish = $("<p class='sleel'>&nbsp;  &nbsp; *</p>");
+                       syun.append(astxanish);
+                   }
+
+                   if(i > 0 && i < this.a - 1){
+                       if(j == 1){
+                           let astxanish = $("<p class='sleel'>&nbsp;  &nbsp; *</p>");
+                           syun.append(astxanish);
+                       }
+
+                       if(j > 1 && j < this.b){
+                           let astxanish = $("<p class='sleel'>&nbsp;  &nbsp; </p>");
+                           syun.append(astxanish);
+                       }
+
+                       if(j == this.b){
+                           let astxanish = $("<p class='sleel'>&nbsp;  &nbsp; *</p>");
+                           syun.append(astxanish);
+                       }
+                   }
+
+                   if(i == this.a - 1){
+                       let astxanish = $("<p class='sleel'>&nbsp;  &nbsp; *</p>");
+                       syun.append(astxanish);
+                   }
+                }
+                div.append(syun);
+
+            }
             $("body").append(div);
         }
     }
 
-    let u1 = new Uxankyun(150, 300);
-    console.log(u1.makeres())
-    console.log(u1.paragic())
-    u1.draw()
+    let u1 = new Uxankyun(8, 4);
+    console.log(u1.makeres());
+    console.log(u1.paragic());
+    u1.draw();
 
+    class InputType{
+        constructor(type) {
+            this.type = type
+        }
+    }
+
+    class CreateInput extends InputType{
+        constructor(type){
+            super(type)
+        }
+
+        creat(){
+            let inp = $(`<input type="${this.type}">`)
+            $("body").append(inp)
+        }
+    }
+
+    let inp1 = new CreateInput("text")
+    inp1.creat()
 })
-
-// class CreateInput{
-//     create(){
-//         let input = document.createElement("input")
-//         input.type = "text"
-//         document.body.append(input)
-//     }
-// }
-//
-// let x = new CreateInput()
-// x.create()
--- JavaScript/HTMLJS jQuery Կրկնություն-lesson 18/JS lesson 18/js/contactJS.js
+++ JavaScript/HTMLJS jQuery Կրկնություն-lesson 18/JS lesson 18/js/contactJS.js
@@ -5,39 +5,19 @@ $(".sendBtn").click((e) => {
         let emailInput = $(".contactEmailInput").val();
         let phoneInput = $(".contactPhoneInput").val();
         let messageTextera = $(".contactTextarea").val();
-        console.log(nameInput, emailInput, phoneInput, messageTextera)
 
         if(phoneInput == "" && nameInput == "" && phoneInput == "" && messageTextera == ""){
             $(".contInp").addClass("borderInpur")
-
-
-            $(".phoneInputSpan, .nameInputSpan").css({
-                fontSize:"12px",
-                color:"red",
-                textAlign:"right"
-            })
-
-            $(".phoneInputSpan").html("Only numbers are required");
-            $(".nameInputSpan").html("The text field is required.")
-
-        } else if (phoneInput == ""){
-            $(".phoneInput").css("border-bottom", "1px solid red");
-            $(".phoneInputSpan").css({
-                fontSize:"12px",
-                color:"red",
-                textAlign:"right"
-            })
-
-            $(".phoneInputSpan").html("Only numbers are required");
-        } else if(nameInput == ""){
-            $(".nameInput").css("border-bottom", "1px solid red");
-            $(".nameInputSpan").css({
-                fontSize:"12px",
-                color:"red",
-                textAlign:"right"
-            })
-
-            $(".nameInputSpan").html("The text field is required.");
+            $(".spanInput").addClass("spanInputShow")
+        } else if (nameInput == ""){
+            $(".contInpNam").addClass("borderInpur");
+            $(".contSpanNam").addClass("spanInputShow")
+        } else if(emailInput == ""){
+            let rgEmail = /^[^@\s\t\r\n] + @[^@\s\t\r\n] + \.[^@\s\t\n\r]/
+            console.log(rgEmail.test(emailInput))
+            console.log(emailInput)
+            $(".contInpEmail").addClass("borderInpur");
+            $(".contSpanEmail").addClass("spanInputShow");
         }else {
             if(isNaN(Number(phoneInput))) {
                 $(".phoneInput").css("border-bottom", "1px solid red");
